35a0c173603c43441cc6a82a127371da4cb61003
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-03 13:52:44 -0600

    de-concatenate
    Former-commit-id: 0cedee38f4d63f5e75795431aaf880b14094a4a2

---
+++ preprocess.py
@@ -0,0 +1,17 @@
+
+from dendropy import DnaCharacterMatrix
+infi=sys.argv[1]
+outstub=sys.argv[2]
+start=int(sys.argv[3])
+stop=int(sys.argv[4])
+
+orig = DnaCharacterMatrix.get(path="M4058.nexorg", schema="nexus")
+
+d = {}
+for taxon, seq in orig.items():
+	d[taxon.label] = seq.values()[start:stop]
+  
+
+dna = DnaCharacterMatrix.from_dict(d)
+
+dna.write(path="{}.fas".format(outstub), schema="fasta")
