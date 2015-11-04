debe8042b45b70a57d737525415a255955b34d00
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-04 11:33:01 -0600

    script to go from treebase nexus to input fasta
    Former-commit-id: 6f53cb00d8b8f41973ab634e4fc9fe33009b809c

--- preprocess.py
+++ preprocess.py
@@ -7,11 +7,24 @@ stop=int(sys.argv[4])
 
 orig = DnaCharacterMatrix.get(path="M4058.nexorg", schema="nexus")
 
+prune = []
 d = {}
 for taxon, seq in orig.items():
 	d[taxon.label] = seq.values()[start:stop]
+	if len(seq.values()[start:stop].translate(None, "-?")) == 0:
+            prune.append(taxon.label)
   
+for tax in prune:
+	del d[tax]
+
 
 dna = DnaCharacterMatrix.from_dict(d)
 
+tre.prune_taxa_with_labels(prune)
+
 dna.write(path="{}.fas".format(outstub), schema="fasta")
+
+
+ 
+  
+    
