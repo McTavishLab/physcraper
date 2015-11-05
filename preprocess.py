f8863328687f7e290663a138d5d29843a7761bc6
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-05 15:43:11 -0600

    fixed
    Former-commit-id: b628e0304fc18603dd16305d2d6759d1354cf880

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,20 @@
-
+import sys
 from dendropy import DnaCharacterMatrix
 infi=sys.argv[1]
 outstub=sys.argv[2]
 start=int(sys.argv[3])
 stop=int(sys.argv[4])
 
-orig = DnaCharacterMatrix.get(path="M4058.nexorg", schema="nexus")
+orig = DnaCharacterMatrix.get(path=infi, schema="nexus")
 
 prune = []
 d = {}
 for taxon, seq in orig.items():
 	d[taxon.label] = seq.values()[start:stop]
-	if len(seq.values()[start:stop].translate(None, "-?")) == 0:
-            prune.append(taxon.label)
   
-for tax in prune:
-	del d[tax]
 
 
 dna = DnaCharacterMatrix.from_dict(d)
-
-tre.prune_taxa_with_labels(prune)
 
 dna.write(path="{}.fas".format(outstub), schema="fasta")
 
