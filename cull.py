d57010f2e2e3d3986c7bac0fba25e1a7a4170e69
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-24 13:42:52 -0600

    save, then commit
    Former-commit-id: d64e143795683e644ffe4aaf2286fe63055ab7f9

--- cull.py
+++ cull.py
@@ -6,40 +6,31 @@ d = {}
 
 query_seq = DnaCharacterMatrix.get(path="ascomycota.fasta",schema="fasta")
 
-seqs = []
-
-def contains(small, big):
-    for i in xrange(1 + len(big) - len(small)):
-        if small == big[i:i+len(small)]:
-            return 1
-    return 0
-
-
 def seq_dict_build(seq, label, seq_dict):
+    new_seq = seq.symbols_as_string().replace("-","")
     for tax in seq_dict.keys():
-        inc_seq = seq_dict[tax]
-        if len(inc_seq) > len(seq):
-            if contains(seq, ex_seq):
-                sys.stdout.write("seq {} is subsequence of {}, not added".format(label, tax))
+        inc_seq = seq_dict[tax].symbols_as_string().replace("-","")
+        if len(inc_seq) > len(new_seq):
+            if inc_seq.find(new_seq) != -1:
+                sys.stdout.write("seq {} is subsequence of {}, not added\n".format(label, tax))
                 return
         else:
-            if contains(ex_seq, seq):
+            if new_seq.find(inc_seq) != -1:
                 del d[tax]
                 d[label] = seq
-                sys.stdout.write("seq {} is supersequence of {}, {} added and {} removed".format(label, tax, label, tax))
+                sys.stdout.write("seq {} is supersequence of {}, {} added and {} removed\n".format(label, tax, label, tax))
                 return
     print (".")
     d[label] = seq
     return
 
 
-for taxon, seq_in in query_seq.items():
-    seq = seq_in.values()
-    if len(seq) > 800:
+for taxon, seq in query_seq.items():
+    if len(seq.values()) > 800:
         seq_dict_build(seq, taxon.label, d)
     else:
         sys.stdout.write("*")
 
 
 cull = DnaCharacterMatrix.from_dict(d)
-cull.write(path="query_cull.fas".format(runname), schema="fasta")
+cull.write(path="query_cull.fas", schema="fasta")
