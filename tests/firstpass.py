04cc18ac8c102f0d281891f3e2fc0fdab8756e6b
Emily Jane McTavish <ejmctavish@gmail.com>
2016-01-21 14:11:55 -0600

    sync
    Former-commit-id: 3aa61b84b8336055b2cd53f38f53cd7551ab08cc

--- tests/firstpass.py
+++ tests/firstpass.py
@@ -1,4 +1,4 @@
-from physcraper import physcraper_setup, physcraper_scrape
+from physcraper import StudyInfo, PhyscraperSetup, PhyscraperScrape
 import pickle
 
 
@@ -8,23 +8,24 @@ study_id = "pg_873"
 tree_id = "tree1679"
 seqaln = "tests/data/tree1679.fas"
 mattype="fasta"
-runname="new_otus"
+runname="refact"
 
+info = StudyInfo(study_id, tree_id, seqaln, mattype)
 
-test = physcraper_setup(study_id, tree_id, seqaln, mattype, runname)
+test = PhyscraperSetup(info, runname)
 test.setup_physcraper()
 assert(test.mrca_ott == 921280) #changes in the tree could actually change this...
 
 
 pickle.dump(test, open('{}/{}_setup.p'.format(runname,runname), 'wb'))
 
-test2 = physcraper_scrape('{}/{}_setup.p'.format(runname,runname))
-test2.set_date("2016-01-13")
+test2 = PhyscraperScrape('{}/{}_setup.p'.format(runname,runname))
+#test2.set_date("2016-01-13")
 #test2 = pickle.load(open('{}/{}_scrape.p'.format(runname,runname),'rb'))
 #test2.today = "2016-01-13"
 test2.generate_streamed_alignment()
-test2.set_date("2016-01-15")
-test2.generate_streamed_alignment()
+#test2.set_date("2016-01-15")
+#test2.generate_streamed_alignment()
 #test2.mrca_ncbi
 #test2.run_blast()
 #test2._blast_complete = 1
