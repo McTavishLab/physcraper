f5cc13afac1d696290f4f53080541bf04655f6ae
Emily Jane McTavish <ejmctavish@gmail.com>
2016-05-06 14:58:46 -0500

    back to refactory

--- tests/refact.py
+++ tests/refact.py
@@ -1,4 +1,4 @@
-from physcraper import StudyInfo, prune_short, config_obj, IdDicts
+from physcraper import generate_ATT_from_phylesystem, generate_ATT_from_files, prune_short, config_obj, IdDicts,  PhyscraperScrape
 import pickle
 import sys
 import os
@@ -15,13 +15,13 @@ configfi = "tests/local.config"
 
 conf = config_obj(configfi)
 
-info_obj = StudyInfo(seqaln,
+data_obj = generate_ATT_from_phylesystem(seqaln,
                      mattype,
-                     configfi,
+                     "tmp",
                      study_id = study_id,
-                     tree_id = tree_id)
+                     tree_id = tree_id,
+                     phylesystem_loc = 'local')
 
-data_obj = info_obj.generate_ATT()
 
 prune_short(data_obj)
 data_obj.write_files()
@@ -29,3 +29,25 @@ data_obj.write_labelled()
 
 
 ids = IdDicts(conf.ott_ncbi, "tmp")
+
+scraper =  PhyscraperScrape(data_obj, ids, conf)
+scraper.run_blast()
+scraper.read_blast()
+
+
+#otu_json = "tests/minitest_otu.json"
+#treefile = "tests/minitest.tre"
+#info_obj2 = StudyInfo(seqaln,
+#                     mattype,
+                      #"tmp"
+#                     otu_json = otu_json,
+#                     treefile = treefile)
+
+#data_obj2 = info_obj.generate_ATT()
+
+info_obj2 = generate_ATT_from_files("tmp/physcraper.fas",
+                                   mattype,
+                                   "tmp",
+                                   "tmp/physcraper.tre",
+                                    "tmp/otus.json",
+                                   ingroup_mrca=data_obj.ott_mrca)
