92230bc3439f43750e9147fd2afa90cc83829d06
Emily Jane McTavish <ejmctavish@gmail.com>
2016-08-16 12:35:32 -0500

    add pickle to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='Physcraper',
       packages=['physcraper'],
       dependency_links = ['https://github.com/OpenTreeOfLife/peyotl/tarball/physcraper#egg=peyotl-0.1.4devphyscraper'],
       install_requires=[
+          'pickle',
           'dendropy',
           'configparser',
           'biopython',
