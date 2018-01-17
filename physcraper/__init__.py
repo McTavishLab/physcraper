b7df9e8e76fda12badfadf4596f45df7822af3c1
Emily Jane McTavish <ejmctavish@gmail.com>
2018-01-16 17:30:12 -0800

    defensive path checks

--- physcraper/__init__.py
+++ physcraper/__init__.py
@@ -25,6 +25,14 @@ from peyotl.api import APIWrapper
 from urllib2 import URLError
 
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 class ConfigObj(object):
     """Pulls out the configuration information from the config file and makes it easier to pass around and pickle."""
     def __init__(self, configfi):
@@ -32,19 +40,22 @@ class ConfigObj(object):
         config = configparser.ConfigParser()
         config.read(configfi)
         self.e_value_thresh = config['blast']['e_value_thresh']
+        assert is_number(self.e_value_thresh)
         self.hitlist_size = int(config['blast']['hitlist_size'])
         self.seq_len_perc = float(config['physcraper']['seq_len_perc'])
+        assert 0 < self.seq_len_perc < 1
         self.get_ncbi_taxonomy = config['ncbi.taxonomy']['get_ncbi_taxonomy']
+        assert(os.path.isfile(self.get_ncbi_taxonomy))
         self.ncbi_dmp = config['ncbi.taxonomy']['ncbi_dmp']
-        self.phylesystem_loc = config['phylesystem']['location']
-        self.ott_ncbi = config['ncbi.taxonomy']['ott_ncbi']
-        assert os.path.isfile(self.get_ncbi_taxonomy)
         try:
             os.path.isfile(self.ncbi_dmp)
         except:
             os.system("rsync -av ftp.ncbi.nih.gov::pub/taxonomy/gi_taxid_nucl.dmp.gz {}.gz".format(self.config.ncbi_dmp))
             os.system("tar -xzvf taxonomy/gi_taxid_nucl.dmp.gz")
             self.ncbi_dmp = "taxonomy/gi_taxid_nucl.dmp.gz"
+        self.phylesystem_loc = config['phylesystem']['location']
+        assert(self.phylesystem_loc in ['local', 'api'])
+        self.ott_ncbi = config['ncbi.taxonomy']['ott_ncbi']
         assert os.path.isfile(self.ott_ncbi)
 
 
