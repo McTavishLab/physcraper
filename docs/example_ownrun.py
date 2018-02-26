b3f13c05628cac316b4eb8a43a93553f39d788bc
blubbundbla <martha.kandziora@yahoo.com>
2018-02-25 17:56:09 -0800

    new example.py

--- docs/example_ownrun.py
+++ docs/example_ownrun.py
@@ -1,13 +1,13 @@
 from physcraper import wrappers_nonstandard
 #
-seqaln= "docs/owndata/n_infile.fas"
+seqaln= "docs/owndata/senecio_its.fasta"
 mattype="fasta"
-trfn= "docs/owndata/n_RAxML_bestTree.result"
+trfn= "docs/owndata/its_new.tre"
 schema_trf = "newick"
 workdir="docs/owndata/example_owndata_output"
 otujson = "docs/owndata/ott_info_owntree.txt"
 configfi = "example.config"
-idtospname = "docs/owndata/uniquetip_to_name.csv"
+idtospname = "docs/owndata/uniquetip_to_name_its.csv"
 
 
 
