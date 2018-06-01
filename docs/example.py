522c1cbe0d8014cef195b4920d9b1ccc65ff5c6a
Emily Jane McTavish <ejmctavish@gmail.com>
2018-06-01 15:17:52 -0500

    clean up example

--- docs/example.py
+++ docs/example.py
@@ -1,17 +1,32 @@
-from physcraper import wrappers
+import physcraper
+from dendropy import DnaCharacterMatrix
 
 #Use OpenTree phylesystem identifiers to get study and tree
 study_id = "pg_873"
 tree_id = "tree1679"
 seqaln = "tests/data/minitest.fas"
 mattype="fasta"
-workdir="example_output"
+workdir="tests/output/opentree"
 configfi = "example.config"
 
 
-wrappers.standard_run(study_id,
-                      tree_id,
-                      seqaln,
-                      mattype,
-                      workdir,
-                      configfi)
+conf = physcraper.ConfigObj(configfi)
+aln = DnaCharacterMatrix.get(path=seqaln, schema=mattype)
+data_obj = physcraper.generate_ATT_from_phylesystem(aln=aln,
+                                                    workdir=workdir,
+                                                    study_id = study_id,
+                                                    tree_id = tree_id,
+                                                    phylesystem_loc = conf.phylesystem_loc)
+
+
+
+ids =  physcraper.IdDicts(conf, workdir=workdir)
+
+data_obj.prune_short()
+data_obj.write_files()
+
+scraper = physcraper.PhyscraperScrape(data_obj, ids)
+scraper.run_blast()
+scraper.read_blast()
+scraper.remove_identical_seqs()
+scraper.generate_streamed_alignment()
