4bb615754ea873034b6b7b5a3d487eeaa7ea572e
Martha Kandziora <martha.kandziora@yahoo.com>
2018-07-09 17:07:04 -0700

    test update

--- tests/owndata.py
+++ tests/owndata.py
@@ -18,32 +18,32 @@ otu_jsonfi = "{}/otu_dict.json".format(workdir)
 """
 sys.stdout.write("\nTesting 'generate_ATT_from_files'\n")
 try:
-    if not os.path.exists("{}".format(workdir)):
-    	os.makedirs("{}".format(workdir))
+	if not os.path.exists("{}".format(workdir)):
+		os.makedirs("{}".format(workdir))
 
 
-    otu_json = wrappers.OtuJsonDict(id_to_spn, configfi)
-    with open(otu_jsonfi,"w") as outfile:
-        json.dump(otu_json, outfile)
-
-
-
-    data_obj = generate_ATT_from_files(seqaln=seqaln, 
-                                 mattype=mattype, 
-                                 workdir=workdir,
-                                 treefile=trfn,
-                                 schema_trf = schema_trf,
-                                 otu_json=otu_jsonfi,
-                                 ingroup_mrca=None)
+	otu_json = wrappers.OtuJsonDict(id_to_spn, configfi)
+	with open(otu_jsonfi,"w") as outfile:
+		json.dump(otu_json, outfile)
 
 
 
-    print(data_obj.gi_dict)
-    print(data_obj.otu_dict)
+	data_obj = generate_ATT_from_files(seqaln=seqaln,
+								 mattype=mattype,
+								 workdir=workdir,
+								 treefile=trfn,
+								 schema_trf = schema_trf,
+								 otu_json=otu_jsonfi,
+								 ingroup_mrca=None)
 
-    assert isinstance(data_obj, AlignTreeTax):
-    sys.stdout.write("\nTest owndata.py passed\n")
-    os.remove(otu_jsonfi)
+
+
+	print(data_obj.gi_dict)
+	print(data_obj.otu_dict)
+
+	assert isinstance(data_obj, AlignTreeTax):
+	sys.stdout.write("\nTest owndata.py passed\n")
+	os.remove(otu_jsonfi)
 #os.rmdir(workdir)
 except:
-    sys.stdout.write("\nTest owndata.py FAILED\n")
+	sys.stdout.write("\nTest owndata.py FAILED\n")
