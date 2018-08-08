d57c2045ce4decea1a7a6a6cd916b262bbcf3209
Martha Kandziora <martha.kandziora@yahoo.com>
2018-08-08 15:34:54 -0700

    update example files

--- tests/tiny_standard_ownfile.py
+++ tests/tiny_standard_ownfile.py
@@ -1,7 +1,7 @@
 # package import
 import os
 import json
-from physcraper import wrappers, OtuJsonDict
+from physcraper import wrappers, OtuJsonDict, ConfigObj, IdDicts
 
 # define here your files
 seqaln = "tests/data/tiny_test_example/test.fas"
@@ -9,19 +9,25 @@ mattype = "fasta"
 trfn = "tests/data/tiny_test_example/test.tre"
 schema_trf = "newick"
 id_to_spn = r"tests/data/tiny_test_example/test_nicespl.csv"
-workdir="tiny_run_july"
+workdir="tests/output/tiny_standard_own"
 configfi = "tests/data/blubb_localblast.config"
 otu_jsonfi = "{}/otu_dict.json".format(workdir)
 
 
-# setup the run
+if not os.path.exists("{}".format(workdir)):
+        os.makedirs("{}".format(workdir))
+
+conf = ConfigObj(configfi)
+ids = IdDicts(conf, workdir=workdir)
+
+
 if os.path.exists(otu_jsonfi):
+    print("load json")
     otu_json = json.load(open(otu_jsonfi))
 else:
-    otu_json = OtuJsonDict(id_to_spn, configfi)
-    if not os.path.exists(workdir):
-       os.mkdir(workdir)
+    otu_json = OtuJsonDict(id_to_spn, ids)
     json.dump(otu_json, open(otu_jsonfi,"w"))
+
 
 
 # select a wrapper function, depending on what you want to do, see short tutorial:
