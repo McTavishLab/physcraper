712ea5d9e23d339d4201e107eaab07acf4df7c17
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2018-11-05 14:46:38 -0800

    Move some of the testing to pytest.
    pytest has many advantages over creating your own test framework.
     1) it has color: Green OK, Red when failling.
     2) it will automatically show you individual error with parameters, so
     you do not have to re-run things. Example:
    
             def test_config():
                from physcraper import ConfigObj
                configfi = "tests/data/localblast.config"
                conf = ConfigObj(configfi, interactive=False)
        >       assert conf.email == 'ejmctavish@gmail.com'
        E       AssertionError: assert 'bussonniermatthias@gmail.com' == 'ejmctavish@gmail.com'
        E         - bussonniermatthias@gmail.com
        E         + ejmctavish@gmail.com
    
     3) when all test are py-testified, it can auto-discover test.
     4) it has options like --lf (last failed) and --ff (failed first). That
     speed-up iterative development.
     5) it shows warning for things you should pay attention to but are not
     yet errors:
    
         tests/test_configread.py::test_config
          /Users/mbussonnier/dev/physcraper/physcraper/__init__.py:143: DeprecationWarning: You passed a bytestring as `filenames`. This will not work on Python 3. Use `cp.read_file()` or switch to using Unicode strings across the board.
            config.read(configfi)
    
    As you will see it also simplify testign quite a bit as it takes care of
    the try/except by itself as well. And in general the less you have to
    write, the easier it is.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,7 @@ if [ -f output/owndata/ ]; then echo 'Found some!'; fi
 
 python tests/test_remove_identical_seqs.py
 python tests/test_fromfile.py
-python tests/test_configread.py
+py.test tests/test_configread.py
 python tests/test_species_translation.py
 python tests/test_write_labelled.py
 python tests/test_add_all.py
@@ -11,7 +11,7 @@ python tests/test_prune_short.py
 python tests/test_remove_taxa_aln_tre.py
 python tests/test_run_local_blast.py
 python tests/test_sp_d.py
-python tests/test_sp_seq_d.py
+py.test tests/test_sp_seq_d.py
 python tests/test_select_seq_by_local_blast.py
 python tests/test_calc_mean_sd.py
 python tests/test_read_local_blast.py
--- tests/test_configread.py
+++ tests/test_configread.py
@@ -1,16 +1,12 @@
+from __future__ import print_function, absolute_import 
 
-import sys
-from physcraper import ConfigObj
 
 expected_keys = ['seq_len_perc', 'num_threads', 'phylesystem_loc', 'blast_loc', 'gifilename', 'get_ncbi_taxonomy', 'hitlist_size', 'id_pickle', 'ott_ncbi', 'url_base', 'ncbi_dmp', 'email', 'e_value_thresh', 'blastdb']
 
-sys.stdout.write("\nTesting configuration object contents\n")
-try:
+def test_config():
+    from physcraper import ConfigObj
     configfi = "tests/data/localblast.config"
-    conf = ConfigObj(configfi, interactive = False)
+    conf = ConfigObj(configfi, interactive=False)
     assert conf.email == 'ejmctavish@gmail.com'
     assert conf.url_base == None
     assert conf.__dict__.keys() == expected_keys
-    sys.stdout.write("\nTest configread.py passed\n")
-except:
-  sys.stdout.write("\nTest configread.py FAILED (expected, test in progress)\n")
