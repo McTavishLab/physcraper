639906786421c189776248324e3732ca2608406e
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2018-11-07 16:05:15 -0800

    wrap all test in a test function

--- tests/filter_OToL.py
+++ tests/filter_OToL.py
@@ -2,24 +2,26 @@ import os
 import json
 from physcraper import wrappers, OtuJsonDict
 
-# Use OpenTree phylesystem identifiers to get study and tree
-study_id = "pg_873"
-tree_id = "tree1679"
-seqaln = "tests/data/minitest.fas"
-mattype = "fasta"
-workdir = "tests/output/opentree"
-configfi = "example.config"
 
-threshold = 2
-selectby = "blast"
-downtorank = "species"
+def test():
+    # Use OpenTree phylesystem identifiers to get study and tree
+    study_id = "pg_873"
+    tree_id = "tree1679"
+    seqaln = "tests/data/minitest.fas"
+    mattype = "fasta"
+    workdir = "tests/output/opentree"
+    configfi = "example.config"
 
-# select a wrapper function, depending on what you want to do, see short tutorial:
-wrappers.filter_OTOL(study_id,
-                     tree_id,
-                     seqaln,
-                     mattype,
-                     configfi,
-                     threshold,
-                     selectby=selectby,
-                     downtorank=downtorank)
+    threshold = 2
+    selectby = "blast"
+    downtorank = "species"
+
+    # select a wrapper function, depending on what you want to do, see short tutorial:
+    wrappers.filter_OTOL(study_id,
+                         tree_id,
+                         seqaln,
+                         mattype,
+                         configfi,
+                         threshold,
+                         selectby=selectby,
+                         downtorank=downtorank)
--- tests/test_concat_sp_to_keep.py
+++ tests/test_concat_sp_to_keep.py
@@ -10,36 +10,37 @@ pickle_fn = "scrape_checkpoint.p"
 workdir_comb = "tests/output/impl_concat"
 genelist = {"its": {"workdir": workdir_its, "pickle": pickle_fn}, "ets": {"workdir": workdir_ets, "pickle": pickle_fn}}
 
-# get to test status
+def test():
+    # get to test status
 
-concat = Concat(workdir_comb, email)
-for item in genelist.keys():
-    concat.load_single_genes(genelist[item]['workdir'], genelist[item]["pickle"], item)
+    concat = Concat(workdir_comb, email)
+    for item in genelist.keys():
+        concat.load_single_genes(genelist[item]['workdir'], genelist[item]["pickle"], item)
 
-concat.combine()
-concat.sp_seq_counter()
-sp_to_keep = concat.sp_to_keep()
+    concat.combine()
+    concat.sp_seq_counter()
+    sp_to_keep = concat.sp_to_keep()
 
-# print(sp_to_keep.keys())
-#
-# print("tests sp_to_keep")
+    # print(sp_to_keep.keys())
+    #
+    # print("tests sp_to_keep")
 
-counter = 0
-sp_keep = []
-for sp in concat.sp_counter:
-    for gene in concat.sp_counter[sp]:
-        if concat.sp_counter[sp][gene] == 0:
-            sp_keep.append(sp)
+    counter = 0
+    sp_keep = []
+    for sp in concat.sp_counter:
+        for gene in concat.sp_counter[sp]:
+            if concat.sp_counter[sp][gene] == 0:
+                sp_keep.append(sp)
 
-# print(sp_keep)
-# print(sp_to_keep.keys())
-# print(len(sp_keep))
-#
-print(len(sp_to_keep.keys()))
+    # print(sp_keep)
+    # print(sp_to_keep.keys())
+    # print(len(sp_keep))
+    #
+    print(len(sp_to_keep.keys()))
 
-try:
-    assert set(sp_to_keep.keys()) == set(sp_keep)
-    print("tests passed")
-except:
-    print("test fails")
+    try:
+        assert set(sp_to_keep.keys()) == set(sp_keep)
+        print("tests passed")
+    except:
+        print("test fails")
 
