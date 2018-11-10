850cd95bd02a402bc5a45ac3ced797c0d560de8d
Martha Kandziora <martha.kandziora@yahoo.com>
2018-11-09 17:03:28 -0800

    cluster error update

--- tests/venv_error_cluster.sh
+++ tests/venv_error_cluster.sh
@@ -2,5 +2,8 @@ virtualenv -p /usr/bin/python venv-test
 source venv-test/bin/activate
 git clone https://github.com/McTavishLab/physcraper.git
 cd physcraper
-pip install --upgrade setupools
-python setup.py develop 
+pip install --upgrade setuptools
+python setup.py develop
+pip install -r requirements.txt
+python tests/testfilesetup.py
+sh tests/run_tests.sh
