8cbeecf1a59b417e4dd2ebfcb56629d576cfaf4c
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2018-12-05 15:10:38 -0800

    Fix marking concat and slow test independently.
    --runslow and --runconcat options were not independant, and --runslow
    would trigger concat test.
    
    Now also discover also all the test automatically if they start with
    test_

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,24 @@
 import pytest
 
 def pytest_addoption(parser):
+
     parser.addoption("--runslow", action="store_true",
         help="run slow tests")
+    parser.addoption("--runconcat", action="store_true",
+        help="run concat tests")
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    skip_concat = pytest.mark.skip(reason="need --runslow option to run")
+    runslow =  config.getoption("--runslow")
+    runconcat =  config.getoption("--runconcat")
+
+    skip_slow   = pytest.mark.skip(reason="need --runslow   option to run")
+    skip_concat = pytest.mark.skip(reason="need --runconcat option to run")
 
     # skip_test = pytest.mark.skip(reason="skip all for now")
     for item in items:
-        if "slow" in item.keywords:
+        if "slow" in item.keywords and not runslow:
             item.add_marker(skip_slow)
-        if "concat" in item.keywords:
+        if "concat" in item.keywords and not runconcat:
             item.add_marker(skip_concat)
         #elif "fast" not in item.keywords:
         #    item.add_marker(skip_test)
