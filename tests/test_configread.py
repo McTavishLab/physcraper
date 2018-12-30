b48abb43f79a37679b26b54dc44958795c0fe134
Martha Kandziora <martha.kandziora@yahoo.com>
2018-12-30 13:40:07 -0800

    fix typo

--- tests/test_configread.py
+++ tests/test_configread.py
@@ -2,7 +2,7 @@ from __future__ import print_function, absolute_import
 
 
 #expected_keys = ['seq_len_perc', 'num_threads', 'phylesystem_loc', 'ncbi_parser_names_fn', 'ncbi_parser_nodes_fn', 'gb_id_filename', 'unmapped', 'hitlist_size', 'id_pickle', 'blast_loc', 'url_base', 'ott_ncbi', 'email', 'e_value_thresh', 'blastdb']
-expected_keys = ['seq_len_perc', 'num_threads', 'phylesystem_loc', 'ncbi_parser_names_fn', 'ncbi_parser_nodes_fn',  'maxlen', hitlist_size', 'gb_id_filename', 'delay', 'unmapped', 'trim_perc', 'url_base', 'ott_ncbi', 'blast_loc', 'id_pickle', 'email', 'e_value_thresh', 'blastdb']
+expected_keys = ['seq_len_perc', 'num_threads', 'phylesystem_loc', 'ncbi_parser_names_fn', 'ncbi_parser_nodes_fn',  'maxlen', 'hitlist_size', 'gb_id_filename', 'delay', 'unmapped', 'trim_perc', 'url_base', 'ott_ncbi', 'blast_loc', 'id_pickle', 'email', 'e_value_thresh', 'blastdb']
 # expected_keys = ['email', 'e_value_thresh', 'hitlist_size', 'blast_loc', 'blastdb', 'url_base',  'ncbi_parser_nodes_fn', 'ncbi_parser_names_fn', 'num_threads', 'gb_id_filename', 'delay',  'unmapped', 'seq_len_perc', 'seq_len_perc', 'trim_perc', 'phylesystem_loc', 'ott_ncbi', 'id_pickle' ]
 
 
