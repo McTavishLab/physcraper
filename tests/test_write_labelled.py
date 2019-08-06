92a17d9711eddbd1680e10dc0fa2b737af3e30e4
Emily Jane McTavish <ejmctavish@gmail.com>
2019-08-05 20:19:10 -0400

    clea nup

--- tests/test_write_labelled.py
+++ tests/test_write_labelled.py
@@ -16,24 +16,25 @@ def test_write_labelled():
 	data_obj = pickle.load(open("tests/data/precooked/tiny_dataobj.p", 'rb'))
 
 	treepath = 'tests/data/tmp/labelled.tre'
-	alnpath = "tests/data/tmp/labelled.fas"
+	alnpath = 'tests/data/tmp/labelled.fas'
 
 	data_obj.write_labelled(label='^user:TaxonName', filename='labelled', direc='tests/data/tmp/', norepeats = False)
 
-	a =  os.path.isfile(treepath)
-	b = os.path.isfile(alnpath)
-	c = filecmp.cmp(treepath, expected_tree_path)
-	d = filecmp.cmp(alnpath, expected_aln_path)
+	assert os.path.isfile(treepath)
+	assert os.path.isfile(alnpath)
+	assert filecmp.cmp(treepath, expected_tree_path)
+	assert filecmp.cmp(alnpath, expected_aln_path)
+	os.remove(treepath)
+	os.remove(alnpath)
 
 	treepath_ottid = 'tests/data/tmp/labelled_ottid.tre'
-	alnpath_ottid = "tests/data/tmp/labelled_ottid.fas"
+	alnpath_ottid = 'tests/data/tmp/labelled_ottid.fas'
 
 	data_obj.write_labelled(label='^ot:ottId', filename='labelled_ottid', direc='tests/data/tmp/', norepeats = False)
 
-	e = os.path.isfile(treepath_ottid)
-	f = os.path.isfile(alnpath_ottid)
-	g = filecmp.cmp(treepath_ottid, expected_tree_path_ottid)
-	h = filecmp.cmp(alnpath_ottid, expected_aln_path_ottid)
-
-	count = 0
-	assert a*b*c*d*e*f*g*h == 1
+	assert os.path.isfile(treepath_ottid)
+	assert os.path.isfile(alnpath_ottid)
+	assert filecmp.cmp(treepath_ottid, expected_tree_path_ottid)
+	assert filecmp.cmp(alnpath_ottid, expected_aln_path_ottid)
+	os.remove(treepath_ottid)
+	os.remove(alnpath_ottid)
