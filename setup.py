4b15e2fc03b3db08e9f28137739441260e6cfccf
Luna Luisa Sanchez Reyes <sanchez.reyes.luna@gmail.com>
2019-10-04 15:23:14 -0700

    modularity first step

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='Physcraper',
       version='0.1',
       description='Physcraper',
