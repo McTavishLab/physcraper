300b567b0791a45efc1819b0b5b8ca2aa6f24788
Emily Jane McTavish <ejmctavish@gmail.com>
2020-03-24 16:35:01 -0700

    streamline test ssetup

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -1,6 +1,7 @@
 import os
 import json
 import copy
+import filecmp
 from pytest import mark
 from contextlib import contextmanager
 
@@ -250,7 +251,7 @@ def test_run_raxml():
     # # scraper.calculate_boostrap()    
     # assert os.path.exists("{}/RAxML_bootstrap.all{}".format(scraper.workdir, scraper.date))
 
-'''
+
 
 @mark.xfail
 def test_mpi():    
@@ -324,4 +325,36 @@ def test_internal_mpi():
                          "-p", "1", "-f", "a", "-x", "1", "-#", "autoMRE",
                          "-n", "all{}".format(scraper.date)])
     # os.chdir(cwd)
-'''
+
+
+
+
+def test_write_labelled():
+    expected_tree_path = "tests/data/expected_output/labelled.tre"
+    expected_aln_path = "tests/data/expected_output/labelled.fas"
+    expected_tree_path_ottid = "tests/data/expected_output/labelled_ottid.tre"
+    expected_aln_path_ottid = "tests/data/expected_output/labelled_ottid.fas"
+    data_obj = copy.deepcopy(data_obj_base)
+    treepath = 'tests/data/tmp/labelled.tre'
+    alnpath = 'tests/data/tmp/labelled.fas'
+
+    data_obj.write_labelled(label='^user:TaxonName', filename='labelled', direc='tests/data/tmp/', norepeats = False)
+
+    assert os.path.isfile(treepath)
+    assert os.path.isfile(alnpath)
+    assert filecmp.cmp(treepath, expected_tree_path)
+    assert filecmp.cmp(alnpath, expected_aln_path)
+    os.remove(treepath)
+    os.remove(alnpath)
+
+    treepath_ottid = 'tests/data/tmp/labelled_ottid.tre'
+    alnpath_ottid = 'tests/data/tmp/labelled_ottid.fas'
+
+    data_obj.write_labelled(label='^ot:ottId', filename='labelled_ottid', direc='tests/data/tmp/', norepeats = False)
+
+    assert os.path.isfile(treepath_ottid)
+    assert os.path.isfile(alnpath_ottid)
+    assert filecmp.cmp(treepath_ottid, expected_tree_path_ottid)
+    assert filecmp.cmp(alnpath_ottid, expected_aln_path_ottid)
+    os.remove(treepath_ottid)
+    os.remove(alnpath_ottid)
