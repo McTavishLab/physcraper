b2fe4ac337e7028744fffe488376d59ef0e10ca6
Emily Jane McTavish <ejmctavish@gmail.com>
2020-06-23 17:53:35 -0700

    ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'm2r',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
