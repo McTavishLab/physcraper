9485580f910d7a99a79707a37a462e0eebb850d0
snacktavish <ejmctavish@gmail.com>
2021-11-12 13:27:53 -0800

    test to catch search taxon error

--- tests/test_from_phylesystem.py
+++ tests/test_from_phylesystem.py
@@ -26,6 +26,47 @@ def test_generate_ATT_from_phylesystem():
 
 test_generate_ATT_from_phylesystem()
 
+def test_search_taxon():
+    study_id = "ot_350"
+    tree_id = "Tr53296"
+    seqaln = "tests/data/minitest.fas"
+    mattype = "fasta"
+    workdir = "tests/output/opentree"
+    configfi = "tests/data/test.config"
+    search_taxon = "565288"
+    data_obj = physcraper.generate_ATT_from_phylesystem(alnfile=seqaln,
+                                                        aln_schema = mattype,
+                                                        workdir=workdir,
+                                                        configfile=configfi,
+                                                        study_id=study_id,
+                                                        tree_id=tree_id,
+                                                        search_taxon = search_taxon)
+    data_obj == True
+    assert(len(data_obj.tre.leaf_nodes())==4)
+    assert(data_obj.otu_dict['Tl805422']['^ot:ottId'] ==  517518)
+    assert(data_obj.otu_dict['Tl805431']['^ot:originalLabel']=='Dinemasporium_pseudostrigosum_CBS_717.85')
+
+def test_search_taxon_list():
+    study_id = "ot_350"
+    tree_id = "Tr53296"
+    seqaln = "tests/data/minitest.fas"
+    mattype = "fasta"
+    workdir = "tests/output/opentree"
+    configfi = "tests/data/test.config"
+    search_taxon = ["5711835", "4051569"]
+    data_obj = physcraper.generate_ATT_from_phylesystem(alnfile=seqaln,
+                                                        aln_schema = mattype,
+                                                        workdir=workdir,
+                                                        configfile=configfi,
+                                                        study_id=study_id,
+                                                        tree_id=tree_id,
+                                                        search_taxon = search_taxon)
+    data_obj == True
+    assert(len(data_obj.tre.leaf_nodes())==4)
+    assert(data_obj.otu_dict['Tl805422']['^ot:ottId'] ==  517518)
+    assert(data_obj.otu_dict['Tl805431']['^ot:originalLabel']=='Dinemasporium_pseudostrigosum_CBS_717.85')
+
+
 
 @pytest.mark.xfail
 def test_generate_ATT_from_phylesystem_fail():
