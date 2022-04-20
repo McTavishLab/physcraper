0ba340626b90038730aed199af366472bd699256
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-04-20 15:30:01 -0700

    tests for viz

--- physcraper/viz.py
+++ physcraper/viz.py
@@ -6,16 +6,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 from Bio import Phylo
 
-def test():
-    print("It works")
-
-def plot(tree_file, pdf_file, fig_size = (10, 10), font_size = 6):
+def plot(tree_file, pdf_file, fig_size = (7, 7), font_size = 12):
     # read the tree_file
     input_tree = Phylo.read(tree_file, "newick")
     # plot the tree
     # define the font size, it can be a variable later:
     matplotlib.rc('font', size= font_size)
-    # set the size of the figure 
+    # set the size of the figure
     fig = plt.figure(figsize = fig_size, dpi=300)
     # alternatively
     # fig.set_size_inches(10, 40)
