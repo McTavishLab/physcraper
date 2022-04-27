13e80e7cfd614ba28624b09c656bd94202ec5008
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-04-27 13:14:48 -0700

    removing axes around tree plot

--- physcraper/viz.py
+++ physcraper/viz.py
@@ -5,20 +5,23 @@ Function to plot an output tree and save it as pdf
 import matplotlib
 import matplotlib.pyplot as plt
 from Bio import Phylo
+import pylab
 
-def plot(tree_file, pdf_file, fig_size = (7, 7), font_size = 12):
+
+def plot_tree(tree_file, pdf_file, fig_size = (7, 7), font_size = 12):
     # read the tree_file
     input_tree = Phylo.read(tree_file, "newick")
     # plot the tree
     # define the font size, it can be a variable later:
     matplotlib.rc('font', size= font_size)
     # set the size of the figure
-    fig = plt.figure(figsize = fig_size, dpi=300)
+    fig = plt.figure(figsize = fig_size, dpi = 300)
     # alternatively
     # fig.set_size_inches(10, 40)
     # set width of the plot, maybe? explore this later
     # How else can we set this
     axes = fig.add_subplot(1, 1, 1)
+    pylab.axis('off')
     Phylo.draw(input_tree, axes=axes, do_show = False)
     # save as pdf
     plt.savefig(pdf_file, dpi=300)
--- tests/test_viz.py
+++ tests/test_viz.py
@@ -4,6 +4,6 @@ from physcraper import viz
 
 t_file = "pg_55_test/outputs_pg_55tree5864/labelled_pg_55tree5864.tre"
 
-physcraper.viz.plot(tree_file = t_file,
+physcraper.viz.plot_tree(tree_file = t_file,
                     pdf_file = "pg_55_test/labelled_pg_55tree5864.pdf",
                     font_size = 8)
