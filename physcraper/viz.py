0056deffbee2f177698f69bdfa2b8c627c87cb07
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-04-27 13:56:07 -0700

    controlling tip label colors

--- physcraper/viz.py
+++ physcraper/viz.py
@@ -1,27 +1,50 @@
 """
-Function to plot an output tree and save it as pdf
+Function to plot a phylogenetic tree and save it as pdf
 """
 
 import matplotlib
 import matplotlib.pyplot as plt
 from Bio import Phylo
 import pylab
+import csv
 
 
-def plot_tree(tree_file, pdf_file, fig_size = (7, 7), font_size = 12):
+def plot_tree(tree_file,
+              tree_file_format = "newick",
+              pdf_file = "tree.pdf",
+              pdf_size = (7, 7),
+              font_size = 12,
+              node_label_color = "blue",
+              axis = "off",
+              otu_info_file = "None"):
     # read the tree_file
-    input_tree = Phylo.read(tree_file, "newick")
+    input_tree = Phylo.read(tree_file, tree_file_format)
     # plot the tree
     # define the font size, it can be a variable later:
-    matplotlib.rc('font', size= font_size)
+    matplotlib.rc('font', size = font_size, weight = "bold")
+    # define node labels color
+    matplotlib.rc('text', color = node_label_color)
     # set the size of the figure
-    fig = plt.figure(figsize = fig_size, dpi = 300)
+    fig = plt.figure(figsize = pdf_size, dpi = 300)
     # alternatively
     # fig.set_size_inches(10, 40)
     # set width of the plot, maybe? explore this later
     # How else can we set this
     axes = fig.add_subplot(1, 1, 1)
-    pylab.axis('off')
-    Phylo.draw(input_tree, axes=axes, do_show = False)
+    # define the type of axis, default ot "off"
+    pylab.axis(axis)
+    # set tip label colors
+    # read otu info file:
+    # if otu_info_file != "None":
+    #     otu_info =
+    # input_tree = Phylo.read(t_file, "newick")
+    # for tip in input_tree.get_terminals():
+    #     print(tip.name)
+    tip_label_colors = {"Brachychiton_acerifolius_otu376453": "r"}
+    # draw the tree
+    Phylo.draw(input_tree,
+               axes=axes,
+               do_show = False,
+               label_colors = tip_label_colors)
     # save as pdf
     plt.savefig(pdf_file, dpi=300)
--- tests/test_viz.py
+++ tests/test_viz.py
@@ -3,6 +3,8 @@ import physcraper
 from physcraper import viz
 
 t_file = "pg_55_test/outputs_pg_55tree5864/labelled_pg_55tree5864.tre"
+csv_file = "pg_55_test/outputs_pg_55tree5864/otu_info_pg_55tree5864.csv"
+
 
 physcraper.viz.plot_tree(tree_file = t_file,
                     pdf_file = "pg_55_test/labelled_pg_55tree5864.pdf",
